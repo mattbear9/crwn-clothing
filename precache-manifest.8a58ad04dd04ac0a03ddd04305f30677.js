6627f35b0147775c7a83e546f059b94ee9e09454
unknown <mathieu.barthelemy@edu.esiee.fr>
2021-06-08 02:42:39 +0200

    Updates

---
+++ precache-manifest.8a58ad04dd04ac0a03ddd04305f30677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2938baa2295ec33fc5fee445da6422",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "875d96371ef05cb80fdd",
+    "url": "/crwn-clothing/static/css/main.9c66c021.chunk.css"
+  },
+  {
+    "revision": "34da0d64c8e8ebf3e89c",
+    "url": "/crwn-clothing/static/js/2.9336cd6b.chunk.js"
+  },
+  {
+    "revision": "875d96371ef05cb80fdd",
+    "url": "/crwn-clothing/static/js/main.043ddd5c.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
